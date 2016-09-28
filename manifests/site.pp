69057a9450701e906fee580acd0845acbf235753
nolebrink <ben.brinkley@gmail.com>
2016-09-28 08:47:50 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -38,7 +38,7 @@ ini_setting { 'random ordering':
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
-#node default {
+node default {
   # This is where you can declare classes for all nodes.
   # Example:
   #   class { 'my_class': }
@@ -58,4 +58,5 @@ ini_setting { 'random ordering':
   # }
   #include users
  # include skeleton
-#}
+ include nginx
+}
