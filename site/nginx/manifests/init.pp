ff18122fa03154988cbdc2cd7a9e06656eddda49
nolebrink <ben.brinkley@gmail.com>
2016-09-28 13:49:35 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,38 +1,68 @@
 class nginx {
-  File {
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-  }
+case $::osfamily {
+'redhat','debian' : {
+$package = 'nginx'
+$owner = 'root'
+$group = 'root'
+$docroot = '/var/www'
+$confdir = '/etc/nginx'
+$logdir = '/var/log/nginx'
+}
+'windows' : {
+$package = 'nginx-service'
+$owner = 'Administrator'
+$group = 'Administrators'
+$docroot = 'C:/ProgramData/nginx/html'
+$confdir = 'C:/ProgramData/nginx'
+$logdir = 'C:/ProgramData/nginx/logs'
+}
+default : {
+fail("Module ${module_name} is not supported on ${::osfamily}")
+}
+}
+# user the service will run as. Used in the nginx.conf.epp template
+$user = $::osfamily ? {
+'redhat' => 'nginx',
+'debian' => 'www-data',
+'windows' => 'nobody',
+}
+File {
+owner => $owner,
+group => $group,
+mode => '0664',
+}
 
-  package { 'nginx':
-    ensure => present,
-  }
+package { $package:
+ensure => present,
+}
+file { [ $docroot, "${confdir}/conf.d" ]:
+ensure => directory,
+}
+file { "${docroot}/index.html":
+ensure => file,
+source => 'puppet:///modules/nginx/index.html',
+}
+file { "${confdir}/nginx.conf":
+ensure => file,
+content => epp('nginx/nginx.conf.epp',
+{
+user => $user,
+confdir => $confdir,
+logdir => $logdir,
+}),
+notify => Service['nginx'],
+}
+file { "${confdir}/conf.d/default.conf":
+ensure => file,
+content => epp('nginx/default.conf.epp',
+{
+docroot => $docroot,
+}),
 
-  file { '/var/www':
-    ensure  => directory,
-    require => Package['nginx'],
-  }
-
-  file { '/var/www/index.html':
-    ensure => file,
-    source => 'puppet:///modules/nginx/index.html',
-  }
-
-  file { '/etc/nginx/nginx.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
-  }
-
-  file { '/etc/nginx/conf.d/default.conf':
-    ensure  => file,
-    source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    subscribe => File['/etc/nginx/conf.d/default.conf', '/etc/nginx/nginx.conf'],
-  }
+notify => Service['nginx'],
+}
+service { 'nginx':
+ensure => running,
+enable => true,
+}
 }
