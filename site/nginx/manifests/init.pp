59b3f5f1aee225d9c293294470908e8d90a6a511
nolebrink <ben.brinkley@gmail.com>
2016-09-28 08:39:17 -0500

    Create init.pp

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,32 @@
+class nginx {
+  package { 'nginx':
+    ensure => present,
+  }
+
+  file { '/var/www':
+    ensure  => directory,
+    require => Package['nginx'],
+  }
+
+  file { '/var/www/index.html':
+    ensure => file,
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
+  file { '/etc/nginx/nginx.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/nginx.conf',
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure  => file,
+    source  => 'puppet:///modules/nginx/default.conf',
+    require => Package['nginx'],
+  }
+
+  service { 'nginx':
+    ensure    => running,
+    subscribe => File['/etc/nginx/conf.d/default.conf', '/etc/nginx/nginx.conf'],
+  }
+}
