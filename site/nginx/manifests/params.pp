4795c5ca23c7d110d55c5f6ad03ac98c8a0f7e8b
nolebrink <ben.brinkley@gmail.com>
2016-09-29 10:14:45 -0500

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,29 +1,35 @@
 class nginx::params {
-case $::osfamily {
-'redhat','debian' : {
-$package = 'nginx'
-$owner = 'root'
-$group = 'root'
-$docroot = '/var/www'
-$confdir = '/etc/nginx'
-$logdir = '/var/log/nginx'
-}
-'windows' : {
-$package = 'nginx-service'
-$owner = 'Administrator'
-$group = 'Administrators'
-$docroot = 'C:/ProgramData/nginx/html'
-$confdir = 'C:/ProgramData/nginx'
-$logdir = 'C:/ProgramData/nginx/logs'
-}
-default : {
-fail("Module ${module_name} is not supported on ${::osfamily}")
-}
-}
-$user = $::osfamily ? {
-'redhat' => 'nginx',
-'debian' => 'www-data',
-'windows' => 'nobody',
-}
-$port = '80'
+  case $::osfamily {
+    'redhat','debian' : {
+      $package  = 'nginx'
+      $owner    = 'root'
+      $group    = 'root'
+      $docroot  = '/var/www'
+      $confdir  = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'           
+    }
+    'windows' : {
+      $package  = 'nginx-service'
+      $owner    = 'Administrator'
+      $group    = 'Administrators'
+      $docroot  = 'C:/ProgramData/nginx/html'
+      $confdir  = 'C:/ProgramData/nginx/conf'
+      $blockdir = 'C:/ProgramData/nginx/conf.d' 
+    }
+    default   : {
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
+
+  case $::osfamily {
+    'redhat': {
+      $run_user = 'nginx'
+    }
+    'debian': {
+      $run_user = 'www-data'
+    }
+    'windows': {
+      $run_user = 'nobody'
+    }
+  }
 }
